cc1cf0bb4bdfdd2b7c617b9f475ce71d7862f4f5
wanghaisheng <edwin.uestc@gmail.com>
2024-05-18 01:47:05 +0800

    fix amz

--- src/amz.py
+++ src/amz.py
@@ -22,57 +22,36 @@ def getSellerJson(sellerid):
         # time.sleep(lag)
         surl = url + sellerid + "?domain=1&seller=" + sellerid
         # print(f"try without proxy {sellerid}")
+        try:
+            response = requests.get(
+                surl,
+                headers=headers,
+                # proxies=proxies # 如果需要代理，取消注释并设置正确的代理
+
+                                                # , verify=False
+
+        )
+            response.raise_for_status()  # 这将为4XX和5XX的响应抛出异常
+
+            data = response.json()
+        
+            try:
+                data['sellers'][sellerid]
+
+                print(f"Data saved for sellerid: {sellerid}")
+
+                return json.dump(data, ensure_ascii=False, indent=4)
 
 
-                # valid_proxies.remove(pro_str)
-        max_retries = 5
-
-        proxies=None
-        pro_str=None
-        for i in range(max_retries):
+# 保存更新后的代理列表
+            except:
+                print('no seller json')
+                raise
                 
+        except Exception as e:  # 捕获请求相关的异常
+        # print(f"Attempt failed with error: {e}")
 
-                try:
-                    response = requests.get(
-                        surl,
-                        headers=headers,
-                        # proxies=proxies # 如果需要代理，取消注释并设置正确的代理
-
-                                                        # , verify=False
-
-                )
-                    response.raise_for_status()  # 这将为4XX和5XX的响应抛出异常
-
-                    data = response.json()
-                
-                    try:
-                        data['sellers'][sellerid]
-
-                        print('found seller with proxy',pro_str)
-                        print(f"Data saved for sellerid: {sellerid}")
-
-                        return json.dump(data, ensure_ascii=False, indent=4)
-
-
-        # 保存更新后的代理列表
-                    except:
-                        print('no seller json')
-                        raise
-                        
-                except Exception as e:  # 捕获请求相关的异常
-                # print(f"Attempt failed with error: {e}")
-
-                        print(f"Attempt {i+1} failed with error:{e}")
-
-                        if i < max_retries - 1:
-                                # time.sleep(2**i)  # 指数退避策略
-                                print("Retrying...")
-                                continue  # 继续下一次重试
-                        else:
-                                print(
-                                "Max retries reached. Exiting without a successful response."
-                                )
-                                break  # 最大重试次数达到，退出循环
+            return None
 
 
 def get_proxy():
